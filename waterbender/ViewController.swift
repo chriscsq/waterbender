732d351ac62c7b95a5ce2d027600906c539c6b8d
chrischau94@gmail.com <chrischau94@gmail.com>
2019-02-16 15:11:09 -0700

    Initial Commit

---
+++ waterbender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  waterbender
+//
+//  Created by Chris Chau on 2019-02-16.
+//  Copyright © 2019 bigheartsmallbrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
