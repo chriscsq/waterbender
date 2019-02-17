ba8497f3da3bb0ec38bf5586547c8d83ba2d6f3e
chrischau94@gmail.com <chrischau94@gmail.com>
2019-02-16 19:08:58 -0700

    fixed split screen issue

--- waterbender/ViewController.swift
+++ waterbender/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "sea.png")!)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
