4947cc96ebf30b5167380e981180f78990b26637
luke.iremadze <luke.iremadze@mac06-ec.local>
2019-02-16 21:13:32 -0800

    Monthly page view added

--- waterbender/ViewController.swift
+++ waterbender/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sea: UIImageView!
+    @IBOutlet weak var monthlyBtn: UIButton!
+    @IBOutlet weak var monthlyLbl: UILabel!
+    @IBOutlet weak var weaklyBtn: UIButton!
+    @IBOutlet weak var weaklyLbl: UILabel!
+    @IBOutlet weak var dailyBtn: UIButton!
+    @IBOutlet weak var dailyLbl: UILabel!
+    @IBOutlet weak var monthlyHolder: UIView!
+    @IBOutlet weak var monthlyNav: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func actionBtnPressed(_ sender: Any) {
+        monthlyBtn.isHidden = true
+        monthlyLbl.isHidden = true
+        weaklyBtn.isHidden = true
+        weaklyLbl.isHidden = true
+        dailyBtn.isHidden = true
+        dailyLbl.isHidden = true
+        sea.isHidden = false
+        
+        
+        
+    }
+    
+/*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+ */
 
 }
 
