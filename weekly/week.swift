8da85a86b5d25a1e68408525bb6af7e690ab1920
Simran Bhattarai <simran.bhattarai@mac06-ec.local>
2019-02-17 10:21:30 -0800

    Attempt to make chart

---
+++ weekly/week.swift
@@ -0,0 +1,13 @@
+//
+//  week.swift
+//  waterbenderTests
+//
+//  Created by Simran Bhattarai on 2/17/19.
+//  Copyright © 2019 bigheartsmallbrain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
